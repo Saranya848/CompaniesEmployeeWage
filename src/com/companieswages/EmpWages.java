405deb1eeb8beedf00f81cd9db3f1882fc38c162
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-02-25 00:56:52 +0530

    Employee parttime wages

--- src/com/companieswages/EmpWages.java
+++ src/com/companieswages/EmpWages.java
@@ -3,21 +3,20 @@ package com.companieswages;
 public class EmpWages {
 	public static void empdata(int fulltime, int parttime) {
 		final int wagesperhrs = 20;
-		int empHours = 0;
+		int empHours = 0, ftempHours = 16, ptempHours = 8;
 		// number for 0 for absent 1 for present
 		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
 		// System.out.println(empCheck);
 
 		// if condition to check employee is working full time, part time or absent
 		if (empCheck == fulltime)
-			empHours = 8;
+			System.out.println("Employee Wage is " + wagesperhrs * ftempHours);
 		else if (empCheck == parttime)
-			empHours = 4;
+
+			// Calculating daily wages for part time
+			System.out.println("Employee Wage is " + wagesperhrs * ptempHours);
 		else
-			empHours = 0;
-		// Calculating daily wages
-		int dailywages = wagesperhrs * empHours;
-		System.out.println("Employee Wage is " + dailywages);
+			System.out.println("Employee Wage is " + wagesperhrs * empHours);
 
 	}
 
