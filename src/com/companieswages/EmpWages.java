6c7687e755f76b40cd274a3e54ab7a083403f4a7
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-02-27 22:42:02 +0530

    Calculate total wage

--- src/com/companieswages/EmpWages.java
+++ src/com/companieswages/EmpWages.java
@@ -13,11 +13,17 @@ public class EmpWages {
 
 	public void empdata() {
 		int empHours = 0, dailywages = 0, monthlyWage = 0;
-		// number for 0 for absent 1 for present
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		// System.out.println(empCheck);
+
 		// looping for n working days
-		for (int day = 1; day <= workingday; day++) {
+		int totalempHrs = 0, totalWage = 0;
+		int totalworkingdays = 0;
+		int workinghours = 100;
+
+		while (totalempHrs <= workinghours && totalworkingdays <= workingday) {
+			totalworkingdays++;
+			// number for 0 for absent 1 for present
+			int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+			// Switch to check employee is working full time, part time or absent
 			switch (empCheck) {
 			case 0:
 				empHours = 0;// Employee absent
@@ -29,25 +35,29 @@ public class EmpWages {
 				empHours = 8;// Employee Fulltime present
 				break;
 			}
-			// Calculating Daily Wages and Monthly Wages of employee
+			// Calculating Daily Wages, Monthly Wages of employee
+
 			dailywages = wagesperhrs * empHours;
 			monthlyWage = monthlyWage + dailywages;
+			// Total Hour calculation
+			totalempHrs = totalempHrs + empHours;
 		}
-		System.out.println("Employee Wage daily is " + dailywages + " and Monthly Wage is " + monthlyWage);
+		System.out.println("Employee dailywages is " + dailywages + ", Monthly wages is " + monthlyWage);
+		System.out.println("Employee total Salary" + totalWage + " and total Hours is " + totalempHrs);
 	}
 
 	public static void main(String[] args) {
 
 		// declaring constants
 		Scanner sc = new Scanner(System.in);
-			System.out.println("Enter Rate per hour value:");
-			int wagesperhrs = sc.nextInt();
-			System.out.println("Enter Working Days value:");
-			int workinghours = sc.nextInt();
-			System.out.println("Enter Max Allowed working hours value:");
-			int workingday = sc.nextInt();
-			sc.close();
-			EmpWages company = new EmpWages(wagesperhrs, workinghours, workingday); // Object
-			company.empdata();
+		System.out.println("Enter Rate per hour value:");
+		int wagesperhrs = sc.nextInt();
+		System.out.println("Enter Working Days value:");
+		int workinghours = sc.nextInt();
+		System.out.println("Enter Max Allowed working hours value:");
+		int workingday = sc.nextInt();
+		sc.close();
+		EmpWages company = new EmpWages(wagesperhrs, workinghours, workingday); // Object
+		company.empdata();
 	}
 }
